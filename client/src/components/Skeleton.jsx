601e4dc7f36ab22530ad13fc0eefd29ce120cc33
Daniel Bělík <the.drobak@gmail.com>
2026-05-07 03:33:47 +0200

    feature/GatewaySkeleton and ButtonSkeleton

---
+++ client/src/components/Skeleton.jsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "./ui/skeleton";
+import { Card, CardContent, } from "./ui/card";
+
+export function GatewaySkeleton() {
+    return(
+        <>
+            <Card >
+                <CardContent className="mt-2 flex flex-col items-start justify-between gap-2">
+                    <Skeleton className="h-4 w-1/4" />
+                    <Skeleton className="h-2 w-[40px]" />
+                    <Skeleton className="mt-4 h-[80px] w-full" />
+                </CardContent>
+            </Card>
+        </>
+    )
+}
+
+export function ButtonSkeleton({ className }) {
+    return(
+        <Skeleton className={`mb-4 h-[41px] w-[164px] rounded-full ${className}`}/>
+    )
+}
